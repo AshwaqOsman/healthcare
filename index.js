0f327dc48a06dca72623c4513f3da9efc211099b
AshwaqOsman <67389973+AshwaqOsman@users.noreply.github.com>
2024-02-27 22:10:39 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+/*let menu = document.querySelector('#menu-btn');
+let navBar = document.querySelector(".navBar");
+
+
+
+menu.onclick = ()=>{
+    menu.classList.toggle('fa-times');
+    navBar.classList.toggle('active');
+}
+window.scroll =()=>{
+    menu.classList.remove('fa-times');
+    navBar.classList.remove('active');
+   
+
+}*/
+
+var sidemenu = document.querySelector(".side-menu");
+var bars= document.querySelector(".fa-solid");
+function penMenu(){
+    sidemenu.style.display ="block"
+}
+function closemenu(){
+    sidemenu.style.display ="none"
+}
+function showsideBar(){
+    sidemenu.style.display ="block"
+
+}
+
+
+
